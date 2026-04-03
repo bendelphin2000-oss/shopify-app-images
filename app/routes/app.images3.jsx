158beb0ceb6717dba8dfbd5eeb4d3af50de1eeb9
bendelphin2000 <bendelphin2000@gmail.com>
2026-04-03 20:43:52 +0200

    añado nuevo componente Gallery

--- app/routes/app.images3.jsx
+++ app/routes/app.images3.jsx
@@ -1,15 +1,27 @@
 import { useLoaderData } from "react-router";
+import { authenticate } from "../shopify.server";
 
-export async function loader() {
-  return { mensaje: "Hola desde loader 🚀" };
+export async function loader({ request }) {
+  try {
+    const { admin } = await authenticate.admin(request);
+
+    return { status: "AUTH OK" };
+
+  } catch (e) {
+    return {
+      status: "AUTH ERROR",
+      error: e.message,
+    };
+  }
 }
 
-export default function ImagesPage() {
+export default function Page() {
   const data = useLoaderData();
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>{data.mensaje}</h1>
+      <h1>{data.status}</h1>
+      {data.error && <pre>{data.error}</pre>}
     </div>
   );
 }
