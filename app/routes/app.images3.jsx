b645a3e3314fdf2200c3d8ae2d0e569610586063
bendelphin2000 <bendelphin2000@gmail.com>
2026-04-03 20:20:22 +0200

    añado nuevo componente Gallery

--- app/routes/app.images3.jsx
+++ app/routes/app.images3.jsx
@@ -1,8 +1,15 @@
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  return { mensaje: "Hola desde loader 🚀" };
+}
+
 export default function ImagesPage() {
+  const data = useLoaderData();
+
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Hola 👋</h1>
-      <p>La app funciona correctamente</p>
+      <h1>{data.mensaje}</h1>
     </div>
   );
 }
